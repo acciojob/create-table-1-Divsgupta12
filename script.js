458af1db0efefeed8342844384de5f73ed298747
Divya Gupta <117669677+Divsgupta12@users.noreply.github.com>
2023-06-22 16:58:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function insert_Row() {
     //Write your code here
   
+	let table = document.querySelector("#sampleTable");
+
+
+	let row =document.createElement("tr");
+	row.innerHTML = `<td>New Cell1</td>
+	<td>New Cell2</td>
+	`;
+	table.insertBefore(row,table.firstElementChild);
   
 }
